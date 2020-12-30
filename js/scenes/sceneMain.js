1e2b57b583f6d997b892d315c6dcf4037f16a84c
Joshua Favre <joshuaf@MacBook-Pro.attlocal.net>
2020-12-30 12:05:08 -0600

    initial commit

---
+++ js/scenes/sceneMain.js
@@ -0,0 +1,22 @@
+class SceneMain extends Phaser.Scene {
+	constructor() {
+		super('SceneMain');
+	}
+	preload() {
+		// load our images or sounds
+		
+	}
+	create() {
+		// define our objects
+
+		emitter = new Phaser.Events.EventEmitter();
+		controller = new Controller();
+
+		var mediaManager = new MediaManager({ scene: this });
+
+		var sb = new SoundButtons({ scene: this });
+
+	update() {
+		// constant running loop
+	}
+}
